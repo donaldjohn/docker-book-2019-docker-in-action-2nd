3fd70d8bf20af21d40955cbc103bea8cd29ec45f
Shijian Su <sushijian.iss.nus@gmail.com>
2020-03-30 21:31:43 +0800

    Add scripts of build the dockers

---
+++ docker/ch2_agent.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t shijiansu/ch2_agent -f ch2_agent/Dockerfile ch2_agent
---
+++ docker/ch2_mailer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t shijiansu/ch2_mailer -f ch2_mailer/Dockerfile ch2_mailer
